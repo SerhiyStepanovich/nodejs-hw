c8e20fb5f06bdd8091a0a8665a2f7b9132726e62
Serhiy Stepanovich <bysprint82@gmail.com>
2025-11-15 17:33:42 +0200

    correct models

--- src/models/note.js
+++ src/models/note.js
@@ -5,7 +5,7 @@ const noteSchema = new mongoose.Schema(
   {
     title: { type: String, trim: true, required: true },
     content: { type: String, trim: true, default: '' },
-    tag: { type: String, enum: TAGS, default: null },
+    tag: { type: String, trim: true, enum: TAGS, default: 'Todo' },
   },
   {
     timestamps: true,
