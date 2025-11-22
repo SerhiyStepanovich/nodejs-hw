e193a27aca596232512126491f085c8eb868249f
Serhiy Stepanovich <bysprint82@gmail.com>
2025-11-22 08:32:33 +0200

    04 auth (#10)
    * HW-02-mongodb
    
    * HW-03-validation
    
    * HW-04-auth
    
    * HW-04-auth-corr-conflict
    
    * Fix security vulnerabilities with npm audit fix
    
    * add_authRoutes
    
    * Change default tag value to 'Todo' in note schema

--- src/models/note.js
+++ src/models/note.js
@@ -5,7 +5,7 @@ const noteSchema = new mongoose.Schema(
   {
     title: { type: String, trim: true, required: true },
     content: { type: String, trim: true, default: '' },
-    tag: { type: String, enum: TAGS, default: null },
+    tag: { type: String, enum: TAGS, default: 'Todo' },
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
