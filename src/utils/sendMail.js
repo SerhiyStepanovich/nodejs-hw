22d24d62a06e2e838fee3ee3d4c226ac1bceafe9
Serhiy Stepanovich <bysprint82@gmail.com>
2025-11-28 22:37:03 +0200

    05 mail and img (#17)
    * HW-02-mongodb
    
    * HW-03-validation
    
    * HW-04-auth
    
    * HW-04-auth-corr-conflict
    
    * Fix security vulnerabilities with npm audit fix
    
    * add_authRoutes
    
    * Change default tag value to 'Todo' in note schema
    
    * correct_err
    
    * correct_err
    
    * corr-user.js
    
    * correct_errors
    
    * init_05-mail-and-img
    
    * try
    
    * corr_mail
    
    * Add sendMail utility
    
    * corr_err_03
    
    * corr_rrr_-04

---
+++ src/utils/sendMail.js
@@ -0,0 +1,40 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+import handlebars from 'handlebars';
+import fs from 'fs/promises';
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: process.env.SMTP_PORT === '465',
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+export const sendEmail = async ({ to, subject, template, data }) => {
+  try {
+    const templatePath = `src/templates/${template}.html`;
+    const source = await fs.readFile(templatePath, 'utf8');
+    const compiledTemplate = handlebars.compile(source);
+
+    const html = compiledTemplate(data);
+
+    const emailOptions = {
+      from: process.env.SMTP_FROM,
+      to,
+      subject,
+      html,
+    };
+
+    const info = await transporter.sendMail(emailOptions);
+    console.log('Email sent: %s', info.messageId);
+    return info;
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw new Error('Failed to send the email, please try again later.');
+  }
+};
